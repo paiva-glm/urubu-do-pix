9f0249f945150176c1db4b24c9099dd2ff1ca37c
gu0303 <ga.araujo0303@gmail.com>
2025-06-26 21:36:20 -0300

    Início do projeto

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Minha Aplicação')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+    <header>
+        <h1>Precisa de dinheiro fácil? chama o urubu do pix!</h1>
+    </header>
+
+    <main>
+        @yield('content_body')
+    </main>
+
+    <footer>
+        <p>&copy; 2025 - Todos os direitos reservados.</p>
+    </footer>
+
+</body>
+</html>
