8d805f4a874939beed82aeae8cff6dcb00fbda2c
gu0303 <ga.araujo0303@gmail.com>
2025-06-26 21:51:46 -0300

    Atualização css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,13 @@
 
 @section('content_body')
     <h2>Bem-vindo à página inicial!</h2>
-    <img src="https://imgs.search.brave.com/MPnyCBaDH6lXobGrSZU11F5gSEYhEjT2a0tfZrbheGw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMi10/ZWNodHVkby5nbGJp/bWcuY29tLzI1WnVO/TG5XRTlYVmIyZmt1/VVBRR2dlQ3NuUT0v/MHgwOjQ1MHg0NTAv/OTg0eDAvc21hcnQv/ZmlsdGVyczpzdHJp/cF9pY2MoKS9pLnMz/LmdsYmltZy5jb20v/djEvQVVUSF8wOGZi/ZjQ4YmMwNTI0ODc3/OTQzZmU4NmU0MzA4/N2U3YS9pbnRlcm5h/bF9waG90b3MvYnMv/MjAyMy84L2IvNjVm/djZjU0JDcWk1aFRB/akJFTFEvdXJ1YnUt/ZG8tcGl4LTEucG5n">
-    <p>    As condições de vida desse país deixam as coisas difíceis para qualquer um viver a devida vida que merece. 
-        Por isso eu trago a solução! isso mesmo eu! o urubu do pix, irei pegar uma quantia de dinheiro e aplicarei em diversos meios de investimentos e logo trarei <strong>DEZ VEZES MAIS</strong> para você!
-    </p>
+    <div class="blocoTexto">
+        <img src="https://imgs.search.brave.com/MPnyCBaDH6lXobGrSZU11F5gSEYhEjT2a0tfZrbheGw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMi10/ZWNodHVkby5nbGJp/bWcuY29tLzI1WnVO/TG5XRTlYVmIyZmt1/VVBRR2dlQ3NuUT0v/MHgwOjQ1MHg0NTAv/OTg0eDAvc21hcnQv/ZmlsdGVyczpzdHJp/cF9pY2MoKS9pLnMz/LmdsYmltZy5jb20v/djEvQVVUSF8wOGZi/ZjQ4YmMwNTI0ODc3/OTQzZmU4NmU0MzA4/N2U3YS9pbnRlcm5h/bF9waG90b3MvYnMv/MjAyMy84L2IvNjVm/djZjU0JDcWk1aFRB/akJFTFEvdXJ1YnUt/ZG8tcGl4LTEucG5n">
+        <p>    As condições de vida desse país deixam as coisas difíceis para qualquer um viver a devida vida que merece. 
+        Por isso eu trago a solução! isso mesmo eu! o urubu do pix, irei pegar uma quantia de dinheiro
+        e aplicarei em diversos meios de investimentos e logo trarei <strong>DEZ VEZES MAIS</strong> para você!
+     </p>
+    </div>
 @endsection
 
 
