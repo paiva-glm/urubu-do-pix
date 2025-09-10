771970a88fdb831b840cc1f5db5c6cfa268e1034
gu0303 <ga.araujo0303@gmail.com>
2025-09-10 10:55:37 -0300

    Pequanas alterações css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Página Inicial')
 
 @section('content_body')
-    <h2 style="font-size: 30px; margin-top: 30px; margin-bottom: -30px;">Bem-vindo à página inicial!</h2>
+    <h2 style="font-size: 30px; margin-top: 30px; margin-bottom: -26px;">Bem-vindo à página inicial!</h2>
     <div class="container">
         <img src="https://imgs.search.brave.com/MPnyCBaDH6lXobGrSZU11F5gSEYhEjT2a0tfZrbheGw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMi10/ZWNodHVkby5nbGJp/bWcuY29tLzI1WnVO/TG5XRTlYVmIyZmt1/VVBRR2dlQ3NuUT0v/MHgwOjQ1MHg0NTAv/OTg0eDAvc21hcnQv/ZmlsdGVyczpzdHJp/cF9pY2MoKS9pLnMz/LmdsYmltZy5jb20v/djEvQVVUSF8wOGZi/ZjQ4YmMwNTI0ODc3/OTQzZmU4NmU0MzA4/N2U3YS9pbnRlcm5h/bF9waG90b3MvYnMv/MjAyMy84L2IvNjVm/djZjU0JDcWk1aFRB/akJFTFEvdXJ1YnUt/ZG8tcGl4LTEucG5n">
         <p>    As condições de vida desse país deixam as coisas difíceis para qualquer um viver a devida vida que merece. 
