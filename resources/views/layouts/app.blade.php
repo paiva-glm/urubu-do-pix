7e7a0661f226f49039150354f53212a0ece3aa0f
gu0303 <ga.araujo0303@gmail.com>
2025-09-11 10:20:44 -0300

    botão audio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="icon" href="https://www.shutterstock.com/shutterstock/photos/2372798331/display_1500/stock-vector-vector-illustration-of-vulture-silhouette-2372798331.jpg">
     <title>@yield('title', 'Minha Aplicação')</title>
     @vite('resources/css/app.css')
