3125cc7a71b4616d7292066566dc88e9c78ac6b2
gu0303 <ga.araujo0303@gmail.com>
2025-09-11 10:12:24 -0300

    botão audio

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+const audio = document.getElementById('audio');
+const button = document.getElementById('audio-toggle');
+
+button.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    button.innerHTML = '<i class="fas fa-volume-up"></i>';
+    button.classList.remove('audio-off');
+    button.classList.add('audio-on');
+  } else {
+    audio.pause();
+    button.innerHTML = '<i class="fas fa-volume-off"></i>';
+    button.classList.remove('audio-on');
+    button.classList.add('audio-off');
+  }
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,10 @@
 
 @section('content_body')
     <h2 style="font-size: 30px; margin-top: 30px; margin-bottom: -26px;">Bem-vindo à página inicial!</h2>
+    <button id="audio-toggle" class="audio-off">
+    <i class="fas fa-volume-off"></i>
+    </button>
+    <audio id="audio" src="seu-audio.mp3"></audio>
     <div class="container">
         <img src="https://imgs.search.brave.com/MPnyCBaDH6lXobGrSZU11F5gSEYhEjT2a0tfZrbheGw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMi10/ZWNodHVkby5nbGJp/bWcuY29tLzI1WnVO/TG5XRTlYVmIyZmt1/VVBRR2dlQ3NuUT0v/MHgwOjQ1MHg0NTAv/OTg0eDAvc21hcnQv/ZmlsdGVyczpzdHJp/cF9pY2MoKS9pLnMz/LmdsYmltZy5jb20v/djEvQVVUSF8wOGZi/ZjQ4YmMwNTI0ODc3/OTQzZmU4NmU0MzA4/N2U3YS9pbnRlcm5h/bF9waG90b3MvYnMv/MjAyMy84L2IvNjVm/djZjU0JDcWk1aFRB/akJFTFEvdXJ1YnUt/ZG8tcGl4LTEucG5n"
              style="width: 400px; height: 400px;">
@@ -11,7 +15,7 @@
         Por isso eu trago a solução! isso mesmo eu! o urubu do pix, irei pegar uma quantia de dinheiro
         e aplicarei em diversos meios de investimentos e logo trarei <strong class="rgb-text">DEZ VEZES MAIS</strong> para você!
      </p>
-     <button>Clique aqui!</button>
+     <button style="animation: rgbShadow 3s infinite linear;">Clique aqui!</button>
     </div>
     
 @endsection
